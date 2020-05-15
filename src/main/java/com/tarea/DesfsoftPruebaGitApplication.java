b1ec3e6bd510bb34ce0dc9e29af0f539a12e8f64
erickcanova <erickcn21@gmail.com>
2020-05-14 21:23:33 -0500

    hola

---
+++ src/main/java/com/tarea/DesfsoftPruebaGitApplication.java
@@ -0,0 +1,13 @@
+package com.tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesfsoftPruebaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesfsoftPruebaGitApplication.class, args);
+	}
+
+}
